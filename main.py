7a30ebfa95fc2a2d048b5027c3b7ad715bec09f7
Mosudi I. O. <imosudi@gmail.com>
2026-07-27 21:18:44 +0100

    style: format main.py imports and spacing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from app import app
-
+import config
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host=config.IP_ADDRESS, port=config.PORT)
